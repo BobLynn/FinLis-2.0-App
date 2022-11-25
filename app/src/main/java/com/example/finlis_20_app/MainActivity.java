b6a28f47266030abe76d53ade3837cee3a94ba93
BobLynn <476127332@qq.com>
2022-11-25 17:46:29 +0800

    added PropertyActivity.java and ocrMainActivity.java

--- app/src/main/java/com/example/finlis_20_app/MainActivity.java
+++ app/src/main/java/com/example/finlis_20_app/MainActivity.java
@@ -18,12 +18,23 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button logoutButton, accountmainButton;
+    Button propertyButton, ocrButton;
+
+    Button logoutButton;
+    Button accountmainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        財產界面按鈕
+        propertyButton = (Button)findViewById(R.id.propertyButton);
+        propertyButton.setOnClickListener(this);
+
+//        光學字元辨識按鈕
+        ocrButton = (Button)findViewById(R.id.propertyButton);
+        ocrButton.setOnClickListener(this);
 
 //        登出按鈕
         logoutButton = (Button)findViewById(R.id.logoutBotton);
@@ -40,6 +51,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         switch (view.getId()){
+            case R.id.propertyButton:
+                startActivity(new Intent(this, PropertyActivity.class));
+                break;
+
+            case R.id.ocrButton:
+                startActivity(new Intent(this, ocrMainActivity.class));
+                break;
+
             case R.id.logoutBotton:
                 startActivity(new Intent(this, LoginActivity.class));
                 Toast.makeText(MainActivity.this, "Logout Successful!", Toast.LENGTH_SHORT).show();
