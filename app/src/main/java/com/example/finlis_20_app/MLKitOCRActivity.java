aa9a5755b46807a61ab10085984347142e889b68
BobLynn <476127332@qq.com>
2022-12-06 00:38:37 +0800

    加入開啓相機拍照並顯示照片之功能

--- app/src/main/java/com/example/finlis_20_app/MLKitOCRActivity.java
+++ app/src/main/java/com/example/finlis_20_app/MLKitOCRActivity.java
@@ -1,18 +1,31 @@
 package com.example.finlis_20_app;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.content.Context;
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.graphics.Rect;
-import android.media.Image;
-import android.net.Uri;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.SparseIntArray;
+import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -23,7 +36,7 @@ import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
-import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class MLKitOCRActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -40,18 +53,18 @@ public class MLKitOCRActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_mlkit_ocractivity);
 
 
-        importImageView = (ImageView) findViewById(R.id.importImageView);
+        importImageView = findViewById(R.id.importImageView);
 
-        importImageButton = (Button) findViewById(R.id.importImageButton);
+        importImageButton = findViewById(R.id.importImageButton);
         importImageButton.setOnClickListener(this);
 
-        ocrButton = (Button) findViewById(R.id.ocrButton);
+        ocrButton = findViewById(R.id.ocrButton);
         ocrButton.setOnClickListener(this);
 
-        copyTextButton = (Button) findViewById(R.id.copyTextButton);
+        copyTextButton = findViewById(R.id.copyTextButton);
         copyTextButton.setOnClickListener(this);
 
-        backToMainButton = (Button) findViewById(R.id.backToMainButton);
+        backToMainButton = findViewById(R.id.backToMainButton);
         backToMainButton.setOnClickListener(this);
 
     }
@@ -60,13 +73,16 @@ public class MLKitOCRActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.importImageButton:
-
+                askCameraPermissions();
                 break;
 
             case R.id.ocrButton:
+
+
                 break;
 
             case R.id.copyTextButton:
+
                 break;
 
             case R.id.backToMainButton:
@@ -76,63 +92,35 @@ public class MLKitOCRActivity extends AppCompatActivity implements View.OnClickL
 
     }
 
-    private void recognizeText(InputImage image, Context context, Uri uri){
-
-//        起手式：建立TextRecognizer的執行個體
-        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
-
-//        使用檔案URI匯入圖片
-        try{
-            image = InputImage.fromFilePath(context, uri);
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void askCameraPermissions(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        }else{
+            openCamera();
         }
-
-//        執行傳遞給process方法
-//        「開始」
-        Task<Text> result =
-                recognizer.process(image)
-                        .addOnSuccessListener(new OnSuccessListener<Text>() {
-                            @Override
-                            public void onSuccess(Text visionText) {
-                                // Task completed successfully
-                                // ...
-                            }
-                        })
-                        .addOnFailureListener(
-                                new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        // Task failed with an exception
-                                        // ...
-                                    }
-                                });
-//        「結束」
     }
-
-//    框選已辨識之文字
-    private void processTextBlock(Text result){
-        String resultText = result.getText();
-        for (Text.TextBlock block : result.getTextBlocks()) {
-            String blockText = block.getText();
-            Point[] blockCornerPoints = block.getCornerPoints();
-            Rect blockFrame = block.getBoundingBox();
-            for (Text.Line line : block.getLines()) {
-                String lineText = line.getText();
-                Point[] lineCornerPoints = line.getCornerPoints();
-                Rect lineFrame = line.getBoundingBox();
-                for (Text.Element element : line.getElements()) {
-                    String elementText = element.getText();
-                    Point[] elementCornerPoints = element.getCornerPoints();
-                    Rect elementFrame = element.getBoundingBox();
-                    for (Text.Symbol symbol : element.getSymbols()) {
-                        String symbolText = symbol.getText();
-                        Point[] symbolCornerPoints = symbol.getCornerPoints();
-                        Rect symbolFrame = symbol.getBoundingBox();
-                    }
-                }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == CAMERA_PERM_CODE){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                openCamera();
+            }else {
+                Toast.makeText(this, "Camera permission is required to use camera.", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
+    private void openCamera() {
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camera, CAMERA_REQUEST_CODE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE){
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            importImageView.setImageBitmap(image);
+        }
+    }
 }
